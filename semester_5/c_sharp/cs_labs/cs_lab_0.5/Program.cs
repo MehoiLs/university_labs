368a13cbb988bb1e4892df845c5351accdc9ac41
MehoiL <129596462+MehoiLs@users.noreply.github.com>
2024-10-18 23:23:33 +0300

    CS-1: refactored

--- semester_5/c_sharp/cs_labs/cs_lab_0.5/Program.cs
+++ semester_5/c_sharp/cs_labs/cs_lab_0.5/Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace cs_lab_0._5
 {
-    internal class Program
+    internal static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            Figure square = new Square(3);
+            Figure circle = new Circle(3);
+            Figure triangle = new Triangle(3, 4, 5);
+            
+            Print("Square. S: {0}, P: {1}", square.CalculateArea(), square.CalculatePerimeter());
+            Print("Circle. S: {0}, P: {1}", circle.CalculateArea(), circle.CalculatePerimeter());
+            Print("Triangle. S: {0}, P: {1}", triangle.CalculateArea(), triangle.CalculatePerimeter());
+        }
+
+        private static void Print(string text, params object[] args)
+        {
+            System.Console.WriteLine(text, args);
         }
     }
 }
